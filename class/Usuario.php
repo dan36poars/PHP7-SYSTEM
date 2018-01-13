e17e89be0e60ae3c85f669aac978c812c8915cc7
dan36poars <eldestemido@gmail.com>
2018-01-13 17:13:43 -0200

    Added methods search, list and login

--- class/Usuario.php
+++ class/Usuario.php
@@ -11,10 +11,9 @@ class Usuario
 	private $Result;
 	private $Error;
 
-	public function getResult(){
-		return $this->Result;
-	} 
-
+	/**
+	 * Finding a user by specific Id
+	 **/
 	public function loadById( $id ){
 		$sql = new Mysql();
 
@@ -30,11 +29,59 @@ class Usuario
 			$this->Error = [ 'WS_ERROR', 'Não retornou Dados!' ];
 		}
 	}
+	
+	/**
+	 * getting data from data base with login and password authentication
+	 **/
+
+	public function login($login, $pass){
+		$sql = new Mysql();
+
+		$result = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN AND dessenha = :PASS", array(
+			':LOGIN' => $login,
+			':PASS' => $pass 
+		));
+
+		if ( isset($result[0]) ) {
+			$this->setResult($result[0]);	
+		}else{
+			$this->Result = false;
+			$this->Error = ['WS_ERROR', 'Login e senha incorretos'];
+		}
+	}
 
 	public function __toString(){
 		return json_encode(array(
 			'Result' => $this->getResult()
 		));
+	}
+
+	public function getResult(){
+		return $this->Result;
+	} 
+
+// STATIC METHODS
+
+	/**
+	 * search a user by login
+	 **/
+	public static function search( $login ){
+		$sql = new Mysql();
+
+		return $sql->select("SELECT * FROM tb_users WHERE deslogin LIKE :SEARCH ORDER BY deslogin ASC", array(
+			':SEARCH' => "%".$login."%"
+		));
+	}
+
+	/**
+	 * Obtain a list all users on the data base
+	 **/
+	public static function getList(){
+		$sql = new Mysql();
+
+		$result = $sql->select("SELECT * FROM tb_users ORDER BY deslogin ASC;");
+
+		return $result;
 	}
 
 // PRIVATE METHODS
--- index.php
+++ index.php
@@ -2,16 +2,46 @@
 
 require( './class/config.inc.php' );
 
-$user = new Usuario();
+// Carrega um usuarios buscando pelo Id
+//------------------------------------
 
-$user->loadById(1);
+// $user = new Usuario();
 
-echo $user;
+// $user->loadById(4);
 
+// echo $user;
 
+// echo "<hr>";
 
+// Carrega um lista de todos od usuarios
+//--------------------------------------
 
+// $users = Usuario::getList();
 
+// foreach ($users as $user) {
+	
+// 	foreach ($user as $key => $value) {
+// 		echo $key." : ".$value."</br>";
+// 	}
+// 	echo "<hr>";
+// }
 
+// Carrega um lista de usuarios buscando pelo login
+//--------------------------------------
 
+// $searchs = Usuario::search(  );
+
+// foreach ($searchs as $search) {
+// 	foreach ($search as $key => $value) {
+// 		echo $key." : ".$value."</br>";
+// 	}
+// 	echo "<hr>";
+// }
+
+// Busca o usuario passando senha e login
+//---------------------------------------
+
+$login = new Usuario();
+$login->login('Root', 'user');
+echo $login;
 ?>
